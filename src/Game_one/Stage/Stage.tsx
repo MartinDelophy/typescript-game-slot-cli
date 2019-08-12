9875791d54003e10064818aa08a7fa33d927a37b
haixin.yang <haixin.yang@weimob.com>
2019-08-12 16:21:49 +0800

    feat:draw line

--- src/Game_one/Stage/Stage.tsx
+++ src/Game_one/Stage/Stage.tsx
@@ -1,7 +1,15 @@
 import * as  React from 'react'
 import { Component, Ref } from 'react'
+
 const Style = require('./style.less') 
-console.log(Style)
+
+import config from './config';
+
+const { width, height, split_x, split_y} = config;
+
+const Xsize:number = width/split_x;
+const Ysize:number = height/split_y;
+
 
 
 
@@ -9,23 +17,38 @@ class Stage extends Component<any, any> {
     [x: string]: any;
     width = 1000;
     height = 1000;
+    myRef: any;
     constructor(props:any){
         super(props);
+        this.myRef = React.createRef()
 
     }
-    getRef = (ref: HTMLCanvasElement) =>{
-        let ctx : CanvasRenderingContext2D = ref.getContext("2d")
-        this.setState({ctx})
-    }  
+
     componentDidMount = () => {
-        let ctx: CanvasRenderingContext2D =  this.state.ctx;
-        // ctx.drawImage(, width, height);
+        let ctx = this.myRef!.current!.getContext("2d");
+        this.drawLine(ctx);
+        
     }
+    //画横竖分割线
+    drawLine = (ctx:any) => {
+        for (let i = 0; i < Xsize; i++) {
+            ctx.beginPath();
+            ctx.moveTo(0, i * split_x);
+            ctx.lineTo(width, i * split_x);
+            ctx.stroke();
+        }
+        for (let i = 0; i < Ysize; i++) {
+            ctx.beginPath();
+            ctx.moveTo(i * split_y, 0);
+            ctx.lineTo(i * split_y, height);
+            ctx.stroke();
+        }
 
+    }
     render() {
         return (
             <div>
-                <canvas className={Style.canvasBorder}  ref={this.getRef} width={this.width} height = {this.height} />
+                <canvas className={Style.canvasBorder} ref={this.myRef} width={this.width} height = {this.height} />
             </div>)
     }
 }
---
+++ src/Game_one/Stage/config.tsx
@@ -0,0 +1,6 @@
+export default  {
+    width: 1000,
+    height: 1000,
+    split_x: 20,
+    split_y: 20
+}
